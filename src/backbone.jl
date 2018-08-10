f1df88fbbc209ca887d7238b363a992dbb7a195f
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-08-09 21:08:50 -0400

    :wrench: moving backbone and node to new format

--- src/backbone.jl
+++ src/backbone.jl
@@ -1,46 +1,23 @@
-function search_backbone_by_query(query::AbstractString)
-
-    # Endpoints
-    headers = Mangal.generate_base_header()
-    endpoint = Mangal.api_root * Mangal.api_endpoints.taxonomy
-
-    # Perform the request
-    this_request = HTTP.get(endpoint*query, headers)
-
-    # Return the collection
-    return [Mangal.format_backbone_response(d) for d in JSON.parse.(String(this_request.body))]
-
+function backbone()
+    return search_objects_by_query(
+        Mangal.api_endpoints.taxonomy,
+        nothing,
+        Mangal.format_backbone_response
+    )
 end
 
-function backbone(;count::Int64=200, page::Int64=1, q::Union{AbstractString,Nothing}=nothing)
-    @assert 0 < count <= 1000
-    @assert 0 < page
-
-    # Make the query
-    query = Mangal.generate_base_query(count, page - 1, q)
-    return Mangal.search_backbone_by_query(query)
+function backbone(q::Vector{Pair{String,T}}) where {T <: Any}
+    return search_objects_by_query(
+        Mangal.api_endpoints.taxonomy,
+        q,
+        Mangal.format_backbone_response
+    )
 end
 
 function backbone(name::AbstractString)
-    # Make the query
-    query = Mangal.generate_base_query(1, 0, nothing)
-    query = query * "&name=$(name)"
-
-    # Get results
-    results = Mangal.search_backbone_by_query(query)
-    @assert length(results) == 1
-    return first(results)
+    return first(backbone([Pair("name", name)]))
 end
 
 function backbone(id::Int64)
-    @assert id > 0
-
-    # Make the query
-    query = Mangal.generate_base_query(1, 0, nothing)
-    query = query * "&id=$(id)"
-
-    # Get results
-    results = Mangal.search_backbone_by_query(query)
-    @assert length(results) == 1
-    return first(results)
+    return first(backbone([Pair("id", id)]))
 end
--- src/node.jl
+++ src/node.jl
@@ -1,39 +1,42 @@
-function search_node_by_query(query::AbstractString)
-
-    # Endpoints
-    headers = Mangal.generate_base_header()
-    endpoint = Mangal.api_root * Mangal.api_endpoints.node
-
-    # Perform the request
-    this_request = HTTP.get(endpoint*query, headers)
-
-    # Return the collection
-    return [Mangal.format_node_response(d) for d in JSON.parse.(String(this_request.body))]
-
+function nodes()
+    return search_objects_by_query(
+        Mangal.api_endpoints.node,
+        nothing,
+        Mangal.format_node_response
+    )
 end
 
-function nodes(network::MangalNetwork; count::Int64=5000, page::Int64=1)
-    @assert 0 < count <= 5000
-    @assert 0 < page
-
-    # Make the query
-    query = Mangal.generate_base_query(count, page - 1, nothing)
-    query = query * "&network_id=$(network.id)"
-
-    # Get results
-    results = Mangal.search_node_by_query(query)
-    return results
+function nodes(q::Vector{Pair{String,T}}) where {T <: Any}
+    return search_objects_by_query(
+        Mangal.api_endpoints.node,
+        q,
+        Mangal.format_node_response
+    )
 end
+
+
+function nodes(network::MangalNetwork)
+    query = [Pair("network_id", network.id)]
+    return nodes(query)
+end
+
+function nodes(network::MangalNetwork, query::Vector{Pair{String,T}}) where {T <: Any}
+    push!(query, Pair("network_id", network.id))
+    return nodes(query)
+end
+
+
+function nodes(taxon::MangalReferenceTaxon)
+    query = [Pair("taxo_id", taxon.id)]
+    return nodes(query)
+end
+
+function nodes(taxon::MangalReferenceTaxon, query::Vector{Pair{String,T}}) where {T <: Any}
+    push!(query, Pair("taxo_id", taxon.id))
+    return nodes(query)
+end
+
 
 function node(id::Int64)
-    @assert id > 0
-
-    # Make the query
-    query = Mangal.generate_base_query(1, 0, nothing)
-    query = query * "&id=$(id)"
-
-    # Get results
-    results = Mangal.search_node_by_query(query)
-    @assert length(results) == 1
-    return first(results)
+    return first(nodes([Pair("id", id)]))
 end
