488ccf23e87a9b54585b9182ecea74b57261b97a
autodocs <autodocs>
2018-10-31 19:47:37 +0000

    build based on aef296a

--- dev/search_index.js
+++ dev/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Mangal",
     "title": "Mangal",
     "category": "section",
-    "text": "This manual describes the functionalities of the Mangal.jl package, to query data from species interaction networks. "
+    "text": "This manual describes the functionalities of the Mangal.jl package, to query data from species interaction networks. This package is a wrapper around the new API for the mangal ecological interactions database. It uses [Julia 1.0][jl] to provide a programmatic interface to read the data. Development of this package and the underlying database was funded by the [Canadian Foundation for Innovation][cfi].[cfi]: https://www.innovation.ca/[jl]: https://julialang.org/"
 },
 
 {
