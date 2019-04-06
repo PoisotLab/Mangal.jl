72a9b17137a17c8b75c7997a53863b8b7dc637ab
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-06 18:47:55 -0400

    :books: add docstrings for generated methods

--- src/generators.jl
+++ src/generators.jl
@@ -13,18 +13,36 @@ for mg_type_pair in types_names
     mg_type, mg_singular = mg_type_pair
     mg_plural = Symbol(string(mg_singular)*"s")
     @eval begin
+        """
+            count(::Type{$mg_type}, query::Pair...)
+
+        Returns the number of $mg_type objects that match a query.
+        """
         function count(::Type{$mg_type}, query::Pair...)
             return Mangal.number_of_objects(Mangal._MANGAL_ENDPOINTS[$mg_type], query...)
         end
     end
     if haskey(Mangal._MANGAL_CACHES, mg_type)
         @eval begin
+            """
+                $mg_plural(query::Pair...)
+
+            This function will return objects of type $mg_type according to the query
+            parameters. To accelerate future queries, the objects returned will be cached.
+
+            To get the latest $mg_type records, this function can be called with no arguments.
+            """
             function $mg_plural(query::Pair...)
                 results = search_objects_by_query($mg_type, query...)
                 Mangal.cache(results)
                 return results
             end
 
+            """
+                $mg_singular(id::Int64)
+
+            Returns the object of type $mg_type whose identifier is `id`.
+            """
             function $mg_singular(id::Int64)
                 if haskey(Mangal._MANGAL_CACHES[$mg_type], id)
                     return Mangal._MANGAL_CACHES[$mg_type][id]
@@ -35,11 +53,24 @@ for mg_type_pair in types_names
         end
     else
         @eval begin
+            """
+                $mg_plural(query::Pair...)
+
+            This function will return objects of type $mg_type according to the query
+            parameters.
+
+            To get the latest $mg_type records, this function can be called with no arguments.
+            """
             function $mg_plural(query::Pair...)
                 results = search_objects_by_query($mg_type, query...)
                 return results
             end
 
+            """
+                $mg_singular(id::Int64)
+
+            Returns the object of type $mg_type whose identifier is `id`.
+            """
             function $mg_singular(id::Int64)
                 return first($mg_plural(Pair("id", id)))
             end
