1dedba051b476ef78107ed151827ed7395f54fc2
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-04-06 15:22:13 -0400

    :wrench: doc update

--- docs/build.jl
+++ docs/build.jl
@@ -15,7 +15,7 @@ using Mangal
 using Weave
 using Mustache
 
-_path_elements = ["docs", "src", "data"]
+_path_elements = ["docs", "src", "vignettes"]
 _jmd_files = filter(x -> endswith(x, ".Jmd"), readdir(joinpath(_path_elements...)))
 _files_to_compile = [joinpath(_path_elements..., f) for f in _jmd_files]
 
