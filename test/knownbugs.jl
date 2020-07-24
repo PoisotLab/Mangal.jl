fb05ed330ffbc1e67159e0329479913a2a501581
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-24 11:23:21 -0400

    update test for all networks mentionned in #7

--- test/knownbugs.jl
+++ test/knownbugs.jl
@@ -9,7 +9,7 @@ module MangalTestKnownBugs
     for (idx,netid) in enumerate(_problematic_ids)
         thisnet = Mangal.network(netid)
         @test typeof(thisnet) <: MangalNetwork
-        U = convert(UnipartiteNetwork, copeland) 
+        U = convert(UnipartiteNetwork, thisnet) 
         @test tyepof(U) <: UnipartiteNetwork
     end
 
