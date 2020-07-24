9985a1da7b7b43c52d40d3fdbfb96e126bd18a86
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-07-24 11:29:20 -0400

    Closes #7

--- src/ecologicalnetworks.jl
+++ src/ecologicalnetworks.jl
@@ -46,8 +46,6 @@ function convert(::Type{EcologicalNetworks.UnipartiteNetwork}, interac::Vector{M
 
     for i in interac
         append!(all_object_nodes, [i.from, i.to])
-        @info i.from
-        @info i.to
     end
 
     object_nodes = unique(all_object_nodes)
