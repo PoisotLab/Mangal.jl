58f0daeade27425f989a3acb20d2069b575112ec
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-11-03 13:17:05 -0400

    0-length element bug for backbone

--- src/backbone.jl
+++ src/backbone.jl
@@ -4,5 +4,6 @@
 Returns the backbone entry for a taxon, matched by *exact* name.
 """
 function backbone(name::AbstractString)
-    return first(backbones(Pair("name", name)))
+    q = backbones(Pair("name", name))
+    return isequal(1)(length(q)) ? only(q) : nothing
 end
